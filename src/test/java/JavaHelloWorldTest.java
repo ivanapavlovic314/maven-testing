f9072273661c6b06e59ed86d6de6e71dd48f62ee
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-10 20:35:21 +0100

    JUnit 4 testing - successful

---
+++ src/test/java/JavaHelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+}
